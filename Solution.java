8836439327fde4dd55338de62dd7b2a30b028bfc
Kaiyan2015 <kx58@cornell.edu>
2015-11-01 01:18:54 -0400

    Add file for recursive solution of Letter Combinations of a Phone Number

---
+++ Solution.java
@@ -0,0 +1,47 @@
+public class Solution {
+    private String[] mapping = {"0", "1", "abc", "def", "ghi","jkl", "mno", "pqrs", "tuv","wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> res = new LinkedList<String>();
+        res = recursive(new LinkedList<String>(), digits);
+        return res;
+    }
+        
+    
+    
+    
+    private LinkedList<String> recursive(LinkedList<String> previous, String digits){
+        
+        
+        if(digits.length() == 0){
+            System.out.println("NA");
+            return new LinkedList<String>();
+        }
+        int num = digits.charAt(0) - '0';
+        LinkedList<String> l1 = new LinkedList<String>();
+        
+        if(previous.size() == 0){
+            
+            for(char s : mapping[num].toCharArray()){
+                String tmp = "";
+                tmp += s;
+                l1.add(tmp);
+                //tmp = "";
+            }
+        }
+        else{
+            for(int i = 0; i < previous.size(); ++i){
+                for(char s : mapping[num].toCharArray()){
+                    String tmp = previous.get(i);
+                    tmp += s;
+                    l1.add(tmp);
+                }
+            }
+        }
+        if(digits.length() == 1) return l1;
+        return recursive(l1,digits.substring(1));
+    }
+    
+}
+
+
